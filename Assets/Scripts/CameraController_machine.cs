b193acd6f9cd1c43eaf31fe1e697734300081789
katomasahiro10 <katomasahiro10@gmail.com>
2021-10-02 05:19:58 +0900

    プルリク用に修正
    グローバル変数をUpperCamelCaseに
    使用されていない変数を削除

--- Assets/Scripts/CameraController_machine.cs
+++ Assets/Scripts/CameraController_machine.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class CameraController_machine : MonoBehaviour
 {
-    public float length_from_target;
-    public float height_from_target;
-    public GameObject target_obj;
-    public float camera_speed;
+    public float LengthFromTarget;
+    public float HeightFromTarget;
+    public GameObject TargetObject;
+    public float CameraSpeed;
 
-    private Vector3 offset;
     private Vector3 camera_target_position;
-    private Vector3 default_camera_position;
-
     // Start is called before the first frame update
     void Start()
     {
-        offset = this.transform.position - target_obj.transform.position;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        camera_target_position = target_obj.transform.position
-                                 - (target_obj.transform.forward * length_from_target)
-                                 + (target_obj.transform.up * height_from_target);
-        this.transform.position = Vector3.Lerp(this.transform.position, camera_target_position, camera_speed * Time.deltaTime );
-        this.transform.LookAt(target_obj.transform);
+        camera_target_position = TargetObject.transform.position
+                                 - (TargetObject.transform.forward * LengthFromTarget)
+                                 + (TargetObject.transform.up * HeightFromTarget);
+        this.transform.position = Vector3.Lerp(this.transform.position, camera_target_position, CameraSpeed * Time.deltaTime );
+        this.transform.LookAt(TargetObject.transform);
     }
 }
