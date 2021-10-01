10add4fce9ba9f100cc3f52f74f04076e61f783a
ShimamotoRiku <book_inmyisland@icloud.com>
2021-10-01 17:20:52 +0900

    add: マシンの移動のひな形を実装

--- Assets/Scripts/MachineBehavior.cs
+++ Assets/Scripts/MachineBehavior.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MachineBehavior : MonoBehaviour
 {
+    public float Forward;
+    public float Back;
+    public float Rotation;
+
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,21 @@ public class MachineBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Rigidbody>().velocity = transform.forward * Forward; // 常に前進
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody = this.GetComponent<Rigidbody>();
+            rigidbody.AddTorque(new Vector3(0, -0.05f, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody = this.GetComponent<Rigidbody>();
+            rigidbody.AddTorque(new Vector3(0, 0.05f, 0));
+        }
     }
 }
