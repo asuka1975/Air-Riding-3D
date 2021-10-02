5d8a02e0779eb054372317638f1b68c834567de2
katomasahiro10 <katomasahiro10@gmail.com>
2021-10-03 01:58:00 +0900

    コメントなど軽く修正

--- Assets/Scripts/CameraController_machine.cs
+++ Assets/Scripts/CameraController_machine.cs
@@ -31,7 +31,7 @@ public class CameraController_machine : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //現在のTargetObject位置からカメラの目標位置を取得
+        //現在のTargetObjectの位置からカメラの目標位置を取得
         behind_camera_position = TargetObject.transform.position
                                  - (TargetObject.transform.forward * LengthFromTarget)
                                  + (TargetObject.transform.up * HeightFromTarget);
@@ -43,48 +43,35 @@ public class CameraController_machine : MonoBehaviour
                                  + (TargetObject.transform.forward * SideCameraForwardDistance)
                                  - (TargetObject.transform.right * SideCameraLateralDistance)
                                  + (TargetObject.transform.up * SideCameraHeight);
-        Debug.Log(right_camera_position + "," + left_camera_position);
-
-        //カメラがTargetObjectの右側にあるか左側にあるかを取得
-        Vector3 camera_position_vec = (this.transform.position - TargetObject.transform.position).normalized;
 
         //カメラ位置ベクトルをターゲットオブジェクト座標系に変換した際のx方向の値で、カメラが右にあるか左にあるかがわかる
-        if(TargetObject.transform.InverseTransformVector(camera_position_vec).x > 0f)
-        {
-            is_camera_located_right_side = true;
-        }
-        else
-        {
-            is_camera_located_right_side = false;
-        }
+        Vector3 camera_position_vec = (this.transform.position - TargetObject.transform.position).normalized;
+        if(TargetObject.transform.InverseTransformVector(camera_position_vec).x > 0f) { is_camera_located_right_side = true; }
+        else { is_camera_located_right_side = false; }
         if(Math.Abs(TargetObject.transform.InverseTransformVector(camera_position_vec).x) < 0.2f){is_camera_located_behind = true;}
         else{is_camera_located_behind = false;}
 
-        if(is_camera_located_right_side)
-        {
-            Debug.Log("カメラは右側");
-        }
-        else{Debug.Log("カメラは左側");}
-
         //Vector3.Lerpを用いてカメラをなめらかに移動させる
-        if(Input.GetKey("a") ) //カメラを車体左側に振る
+        if(Input.GetKey("a") ) //カメラを右に向ける
         {
             if(is_camera_located_behind || !is_camera_located_right_side)
             {
                 this.transform.position = Vector3.Lerp(this.transform.position, left_camera_position, CameraSpeed * Time.deltaTime );
             }
-            else{
-            this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * 3f * Time.deltaTime );
+            else
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * 3f * Time.deltaTime );
             }
         }
-        else if (Input.GetKey("f") )
+        else if (Input.GetKey("f") ) //カメラを左に向ける
         {
             if(is_camera_located_behind || is_camera_located_right_side)
             {
                 this.transform.position = Vector3.Lerp(this.transform.position, right_camera_position, CameraSpeed * Time.deltaTime );
             }
-            else{
-            this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * 3f * Time.deltaTime );
+            else
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * 3f * Time.deltaTime );
             }
         }
         else{
