f40ad19ef50f1cafdac40001e5eaf3611d62bbc1
katomasahiro10 <katomasahiro10@gmail.com>
2021-10-03 01:49:05 +0900

    カメラをA, Fキーで左右にふる
    変数名など直す

--- Assets/Scripts/CameraController_machine.cs
+++ Assets/Scripts/CameraController_machine.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CameraController_machine : MonoBehaviour
 {
     #region public variables
     public float LengthFromTarget;
     public float HeightFromTarget;
+
+    public float SideCameraLateralDistance;
+    public float SideCameraForwardDistance;
+    public float SideCameraHeight;
     public GameObject TargetObject;
     public float CameraSpeed;
     #endregion
 
     private Vector3 behind_camera_position;
+    private Vector3 right_camera_position;
+    private Vector3 left_camera_position;
+    private bool is_camera_located_right_side;
+    private bool is_camera_located_behind;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +31,65 @@ public class CameraController_machine : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //現在のTargetObject位置からカメラの目標位置を取得
         behind_camera_position = TargetObject.transform.position
                                  - (TargetObject.transform.forward * LengthFromTarget)
                                  + (TargetObject.transform.up * HeightFromTarget);
-        this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * Time.deltaTime );
+        right_camera_position = TargetObject.transform.position
+                                 + (TargetObject.transform.forward * SideCameraForwardDistance)
+                                 + (TargetObject.transform.right * SideCameraLateralDistance)
+                                 + (TargetObject.transform.up * SideCameraHeight);
+        left_camera_position = TargetObject.transform.position
+                                 + (TargetObject.transform.forward * SideCameraForwardDistance)
+                                 - (TargetObject.transform.right * SideCameraLateralDistance)
+                                 + (TargetObject.transform.up * SideCameraHeight);
+        Debug.Log(right_camera_position + "," + left_camera_position);
+
+        //カメラがTargetObjectの右側にあるか左側にあるかを取得
+        Vector3 camera_position_vec = (this.transform.position - TargetObject.transform.position).normalized;
+
+        //カメラ位置ベクトルをターゲットオブジェクト座標系に変換した際のx方向の値で、カメラが右にあるか左にあるかがわかる
+        if(TargetObject.transform.InverseTransformVector(camera_position_vec).x > 0f)
+        {
+            is_camera_located_right_side = true;
+        }
+        else
+        {
+            is_camera_located_right_side = false;
+        }
+        if(Math.Abs(TargetObject.transform.InverseTransformVector(camera_position_vec).x) < 0.2f){is_camera_located_behind = true;}
+        else{is_camera_located_behind = false;}
+
+        if(is_camera_located_right_side)
+        {
+            Debug.Log("カメラは右側");
+        }
+        else{Debug.Log("カメラは左側");}
+
+        //Vector3.Lerpを用いてカメラをなめらかに移動させる
+        if(Input.GetKey("a") ) //カメラを車体左側に振る
+        {
+            if(is_camera_located_behind || !is_camera_located_right_side)
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, left_camera_position, CameraSpeed * Time.deltaTime );
+            }
+            else{
+            this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * 3f * Time.deltaTime );
+            }
+        }
+        else if (Input.GetKey("f") )
+        {
+            if(is_camera_located_behind || is_camera_located_right_side)
+            {
+                this.transform.position = Vector3.Lerp(this.transform.position, right_camera_position, CameraSpeed * Time.deltaTime );
+            }
+            else{
+            this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * 3f * Time.deltaTime );
+            }
+        }
+        else{
+            this.transform.position = Vector3.Lerp(this.transform.position, behind_camera_position, CameraSpeed * Time.deltaTime );
+        }
         this.transform.LookAt(TargetObject.transform);
     }
 }
