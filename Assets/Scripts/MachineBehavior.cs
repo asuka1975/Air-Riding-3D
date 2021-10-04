969b2308328f8a8052faa4cd09cf89642458fe5a
ShimamotoRiku <book_inmyisland@icloud.com>
2021-10-04 21:05:12 +0900

    add: マシンの角速度に上限を設定した

--- Assets/Scripts/MachineBehavior.cs
+++ Assets/Scripts/MachineBehavior.cs
@@ -7,8 +7,12 @@ public class MachineBehavior : MonoBehaviour
     public float Forward;
     public float Back;
     public float Rotation;
+    public float minAngVel;
+    public float maxAngVel;
 
     Rigidbody rigidbody;
+
+    float angVel;
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +32,15 @@ public class MachineBehavior : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rigidbody = this.GetComponent<Rigidbody>();
-            rigidbody.AddTorque(new Vector3(0, -0.05f, 0));
+            rigidbody.AddTorque(new Vector3(0, -Rotation, 0));
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rigidbody = this.GetComponent<Rigidbody>();
-            rigidbody.AddTorque(new Vector3(0, 0.05f, 0));
+            rigidbody.AddTorque(new Vector3(0, Rotation, 0));
         }
+
+        angVel = Mathf.Clamp(rigidbody.angularVelocity.magnitude, minAngVel, maxAngVel);
+        rigidbody.angularVelocity = angVel * rigidbody.angularVelocity.normalized;
     }
 }
