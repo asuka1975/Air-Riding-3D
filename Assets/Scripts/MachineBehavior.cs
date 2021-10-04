e5b5ef44b4a5d4b4dc2e6160f440b66da408934d
ShimamotoRiku <book_inmyisland@icloud.com>
2021-10-05 00:05:59 +0900

    fix: 前進の仕組みをAddForceに変更．前進速度の上限も設定

--- Assets/Scripts/MachineBehavior.cs
+++ Assets/Scripts/MachineBehavior.cs
@@ -7,12 +7,12 @@ public class MachineBehavior : MonoBehaviour
     public float Forward;
     public float Back;
     public float Rotation;
+    public float minVel;
+    public float maxVel;
     public float minAngVel;
     public float maxAngVel;
 
     Rigidbody rigidbody;
-
-    float angVel;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +23,11 @@ public class MachineBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().velocity = transform.forward * Forward; // 常に前進
+        var direction = transform.forward * Forward;
+        //float y = GetComponent<Rigidbody>().velocity.y;
+        //GetComponent<Rigidbody>().velocity = new Vector3(direction.x, y, direction.z);
+        rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.AddForce(direction);
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
@@ -36,11 +40,14 @@ public class MachineBehavior : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
+
             rigidbody = this.GetComponent<Rigidbody>();
             rigidbody.AddTorque(new Vector3(0, Rotation, 0));
         }
 
-        angVel = Mathf.Clamp(rigidbody.angularVelocity.magnitude, minAngVel, maxAngVel);
+        var Vel = Mathf.Clamp(rigidbody.velocity.magnitude, minVel, maxVel);
+        rigidbody.velocity = Vel * rigidbody.velocity.normalized;
+        var angVel = Mathf.Clamp(rigidbody.angularVelocity.magnitude, minAngVel, maxAngVel);
         rigidbody.angularVelocity = angVel * rigidbody.angularVelocity.normalized;
     }
 }
