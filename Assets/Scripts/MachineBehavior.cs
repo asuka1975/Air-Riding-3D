40d62ad17707e03476ccbc0c8e29f365dcec9ff3
ShimamotoRiku <book_inmyisland@icloud.com>
2021-10-08 20:45:05 +0900

    fix: publicなメンバ変数名をキャメルケースに変更

--- Assets/Scripts/MachineBehavior.cs
+++ Assets/Scripts/MachineBehavior.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MachineBehavior : MonoBehaviour
 {
-    public float Forward = 30;
-    public float Back;
+    public float forward = 30;
+    public float back;
 
-    public float Rotation = 10;
+    public float rotation = 10;
     public float floating = 0.5f;
     public float minVel = 0;
     public float maxVel = 10;
@@ -30,16 +30,16 @@ public class MachineBehavior : MonoBehaviour
     {
         rigidbody = this.GetComponent<Rigidbody>();
         var position = rigidbody.position;
-        var direction = transform.forward * Forward;
+        var direction = transform.forward * forward;
 
 
         if (Input.GetKey(KeyCode.Space)) //スペースキーが押されたとき
         {
 
         }
-        else //スペースキーが押されていない時
+        else 
         {
-            //マシンが浮き，前進方向に力を受ける
+            //スペースキーが押されていない時，マシンが浮き，前進方向に力を受ける
             rigidbody.position = new Vector3(position.x, floating, position.z);
             rigidbody.AddForce(direction);
         }
@@ -49,22 +49,22 @@ public class MachineBehavior : MonoBehaviour
            
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow)) //←キーが押されたとき
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rigidbody.AddTorque(new Vector3(0, -Rotation, 0)); //マシンは時計回りのトルクを受ける
+            rigidbody.AddTorque(new Vector3(0, -rotation, 0)); //←キーが押されたとき，マシンは時計回りのトルクを受ける
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            rigidbody.angularVelocity = new Vector3();
+            rigidbody.angularVelocity = new Vector3();　//←キーが押されていない時，マシンの角速度を0にする
         }
 
-        if (Input.GetKey(KeyCode.RightArrow)) //→キーが押されたとき
+        if (Input.GetKey(KeyCode.RightArrow)) 
         {
-            rigidbody.AddTorque(new Vector3(0, Rotation, 0)); //マシンは反時計回りのトルクを受ける
+            rigidbody.AddTorque(new Vector3(0, rotation, 0)); //→キーが押されたとき，マシンは反時計回りのトルクを受ける
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            rigidbody.angularVelocity = new Vector3();
+            rigidbody.angularVelocity = new Vector3();　//→キーが押されていない時，マシンの角速度を0にする
         }
 
         //最大速度，最大角速度でクリッピング
