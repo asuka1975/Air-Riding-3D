9eb8325d6a9cb7ee9ff3c446f6f30c47a27de3e0
katomasahiro10 <katomasahiro10@gmail.com>
2021-10-28 08:46:12 +0900

    feat: マシンのHP管理用スクリプト作成

---
+++ Assets/Scripts/MachineHPManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineHPManager : MonoBehaviour
+{
+    //マシンのHPを管理
+    // マシンのHPを変更する場合はpublicなメソッド (gt)
+    public float HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.HP -= 0.01f;
+        if(HP <= 0.0f)
+        {
+            MachineDestroyedEvent();
+        }
+    }
+
+    void MachineDestroyedEvent()
+    {
+        //HPが0になったら呼び出される
+        //ゲームオーバ、マシンの破壊等
+        Debug.Log("あたしは死んだ。スイーツ(笑)");
+    }
+}
