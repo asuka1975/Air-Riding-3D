cd8f57a183a761f67ff9d5efad3ab831cbe39de3
katomasahiro10 <katomasahiro10@gmail.com>
2021-11-13 22:37:18 +0900

    fix: Delete other child gameObjects
    HACK: このやり方だとマシンが子要素を一つしか持てないので注意

--- Assets/Scripts/ItemScripts/ItemAcquisition.cs
+++ Assets/Scripts/ItemScripts/ItemAcquisition.cs
@@ -10,6 +10,10 @@ public class ItemAcquisition : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            foreach (Transform n in other.gameObject.transform)
+            {
+                Destroy(n.gameObject);
+            }
             Addressables.InstantiateAsync(string.Format("Assets/Prefabs/{0}Equipped.prefab",
                 name.Replace("(Clone)", "")), other.transform);
             Destroy(gameObject);
