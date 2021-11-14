3b70056df0bea11017396c4109cf7dee43827fea
katomasahiro10 <katomasahiro10@gmail.com>
2021-11-14 22:23:35 +0900

    change: change values

--- Assets/Scripts/ItemScripts/ItemManager.cs
+++ Assets/Scripts/ItemScripts/ItemManager.cs
@@ -8,23 +8,23 @@ public class ItemManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int bombItemNum = 3;
-        int cannonItemNum = 3;
-        int recoverItemNum = 3;
+        int bombItemNum = 30;
+        int cannonItemNum = 30;
+        int recoverItemNum = 30;
 
         for (int i = 0; i < bombItemNum; i++) {
             Addressables.InstantiateAsync("Assets/Prefabs/BombItem.prefab", 
-                new Vector3(Random.value * 50, 0.0f, Random.value * 50), 
+                new Vector3(Random.value * 250, 0.5f, Random.value * 250), 
                 this.transform.rotation);
         }
         for (int i = 0; i < cannonItemNum; i++) {
             Addressables.InstantiateAsync("Assets/Prefabs/CannonItem.prefab", 
-                new Vector3(Random.value * 50, 0.0f, Random.value * 50), 
+                new Vector3(Random.value * 250, 0.5f, Random.value * 250), 
                 this.transform.rotation);
         }
         for (int i = 0; i < recoverItemNum; i++) {
             Addressables.InstantiateAsync("Assets/Prefabs/RecoverItem.prefab", 
-                new Vector3(Random.value * 50, 0.0f, Random.value * 50), 
+                new Vector3(Random.value * 250, 0.5f, Random.value * 250), 
                 this.transform.rotation);
         }           
     }
