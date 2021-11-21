6b303000035ca2310a29a841cdf2a98d2dd9a80a
ShimamotoRiku <book_inmyisland@icloud.com>
2021-11-22 02:11:54 +0900

    add: ButtonManager.csを作成

--- Assets/Scripts/MachineSelectScene/StartGame.cs
+++ Assets/Scripts/MachineSelectScene/StartGame.cs
@@ -2,17 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Start : MonoBehaviour
 {
     public void OnClick()
     {
+        Button btn = GetComponent<Button>();
+        btn.interactable = true;
         var machine = new Machine();
 
         StartCoroutine(SceneTransitioner.Transition("CityTrial", machine));
     }
-}
-public class Machine
-{
-    public int Id;
 }
