7add0c0d4e76ac6e8328d77cf78dc38a065e64b6
cb244 <cherica2132@gmail.com>
2022-01-16 02:03:18 +0900

    feat: Bombに爆発とダメージを追加

--- Assets/Scripts/ItemScripts/BombManager.cs
+++ Assets/Scripts/ItemScripts/BombManager.cs
@@ -1,32 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class BombManager : MonoBehaviour
 {
     Rigidbody rb;
     Vector3 force_vector;
+    public float survivalTime = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
-        float force_rand = Random.Range(10f, 50f);
-        force_vector = transform.forward * force_rand * -1f;
-        
-        float side_rand = Random.Range(-45f, 45f);
-        float up_rand = Random.Range(0f, 45f);
-        force_vector = Quaternion.Euler(up_rand, side_rand, 0) * force_vector;
-        
+        float force_rand = Random.Range(10f, 20f);
+        float side_rand = Random.Range(-5f, 5f);
+        force_vector = side_rand * transform.right - force_rand * transform.forward;
         rb.AddForce(force_vector, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
+        survivalTime -= Time.deltaTime;
+        if (survivalTime <= 0)
+        {
+            Addressables.InstantiateAsync(
+                "Assets/Prefabs/ExplosionFieldSmall.prefab",
+                this.transform.position, this.transform.rotation
+            );
+            Destroy(this.gameObject);
+        }
     }
 }
