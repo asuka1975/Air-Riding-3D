8a6b90f9065d0812008cbd1b23351c7357404d77
katomasahiro10 <katomasahiro10@gmail.com>
2022-02-08 16:57:25 +0900

    upadte: rename script

--- Assets/Scripts/Network/ConnectToMasterServer.cs
+++ Assets/Scripts/Network/ConnectToMasterServer.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 using Photon.Pun;
 
 /*
-ボタンをクリックすると適当なゲームサーバに接続する
+ボタンをクリックすると適当なマスターサーバに接続する
 TitleSceneのCanvas/Buttonにアタッチする
 アタッチ後OnClick()に登録する
 */
 
-public class ConnectToServer : MonoBehaviourPunCallbacks
+public class ConnectToMasterServer : MonoBehaviourPunCallbacks
 {
     public void OnClick()
     {
+        Debug.Log("*** try to connect master server.");
         PhotonNetwork.ConnectUsingSettings();
     }
 }
