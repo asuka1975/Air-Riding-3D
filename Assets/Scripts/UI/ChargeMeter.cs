64192aeae7b9e4292a94763c46c9b83186cc22d9
asuka1975 <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-02-21 20:57:48 +0900

    fix: find player object in coroutine and add machineBehaviour null check in Update()
    TODO: commonalize the function FindMachine among UI scripts. In Addition, the current code is not considered the effect of the network object.

--- Assets/Scripts/UI/ChargeMeter.cs
+++ Assets/Scripts/UI/ChargeMeter.cs
@@ -12,12 +12,29 @@ public class ChargeMeter : MonoBehaviour
     void Start()
     {
         _charge = GetComponent<Image>();
+        StartCoroutine("FindMachine");
         _machineBehavior = GameObject.FindWithTag("Player").GetComponent<MachineBehavior>();
+    }
+
+    // TODO commonalize among UI scripts.
+    IEnumerator FindMachine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            var player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                _machineBehavior = player.GetComponent<MachineBehavior>();
+                break;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_machineBehavior == null) return;
         _charge.fillAmount = _machineBehavior.charge / 100;
     }
 }
