5834a75ca3806f387d52686f7ad98eabd6c2df8c
cb244 <cherica2132@gmail.com>
2022-02-25 21:27:36 +0900

    feat: StartGameボタンのSEを追加

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
+    public AudioClip SE_select;
     public void OnClick()
     {
+        StartCoroutine("playAudio", SE_select);
+    }
+
+    IEnumerator playAudio(AudioClip audio)
+    {
+        GetComponent<AudioSource>().PlayOneShot(audio);
+        yield return new WaitForSeconds(audio.length);
         StartCoroutine(SceneTransitioner.Transition("MachineSelectScene", ""));
     }
 }
