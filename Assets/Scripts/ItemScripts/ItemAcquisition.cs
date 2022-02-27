1de45423229d3b4ad214eb5e9381d9280f425d7a
asuka1975 <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-02-27 18:03:21 +0900

    fix: アイテム関連のバグを解決

--- Assets/Scripts/ItemScripts/ItemAcquisition.cs
+++ Assets/Scripts/ItemScripts/ItemAcquisition.cs
@@ -37,16 +37,16 @@ public class ItemAcquisition : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && other.GetComponent<PhotonView>().IsMine)
         {
             foreach (Transform n in other.gameObject.transform)
             {
                 if (n.name.Contains("Equipped"))
                 {
-                    Destroy(n.gameObject);
+                    PhotonNetwork.Destroy(n.gameObject);
                 }
             }
-
+            
             var equipped_item = PhotonNetwork.Instantiate(string.Format("{0}Equipped", name.Replace("(Clone)", "")),
                 other.transform.position + other.transform.forward * itemDatas[name].forward +
                 other.transform.up * itemDatas[name].up,
