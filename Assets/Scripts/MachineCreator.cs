bc3d69fdb9ec19aa0814fe131edb4b868f4a77aa
ShimamotoRiku <book_inmyisland@icloud.com>
2022-03-01 00:14:09 +0900

    update: マシンの初期位置をStartPositionsの中からランダムで選ぶようにした

--- Assets/Scripts/MachineCreator.cs
+++ Assets/Scripts/MachineCreator.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public class MachineCreator : MonoBehaviour
 {
+    private Vector3[] StartPositions = {
+        new Vector3(100, 10, 100),
+        new Vector3(100, 10, 200),
+        new Vector3(200, 10, 200),
+        new Vector3(200, 10, 200),
+        new Vector3(150, 10, 50),
+        new Vector3(250, 10, 150),
+        new Vector3(50, 10, 150),
+        new Vector3(150, 10, 250),
+    };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,8 @@ public class MachineCreator : MonoBehaviour
 
     IEnumerator WaitCameraInitialized()
     {
+        var r = new System.Random();
+        var positionId = r.Next(0, 7);
         while (Camera.main.GetComponent<CameraController_machine>() == null)
         {
             yield return new WaitForSeconds(0.1f);
@@ -23,6 +36,6 @@ public class MachineCreator : MonoBehaviour
         //生成するマシンのIDを取得
         var id = GameObject.FindWithTag("SharedParams").GetComponent<SharedParams>().Get<MachineSelectData>().id;
         string machine_name = "Machine" + id;
-        PhotonNetwork.Instantiate(machine_name , new Vector3(100, 10, 100), Quaternion.identity);
+        PhotonNetwork.Instantiate(machine_name , StartPositions[positionId], Quaternion.identity);
     }
 }
