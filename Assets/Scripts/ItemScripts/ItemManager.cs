588659fe017159c6ead38f5f7e26401ee8fa1d84
ShimamotoRiku <book_inmyisland@icloud.com>
2022-03-06 21:02:07 +0900

    fix: アイテムの出現範囲を調整

--- Assets/Scripts/ItemScripts/ItemManager.cs
+++ Assets/Scripts/ItemScripts/ItemManager.cs
@@ -12,19 +12,22 @@ public class ItemManager : MonoBehaviour
         int cannonItemNum = 30;
         int recoverItemNum = 5;
 
+        float range = 80; // アイテムの出現する範囲
+        float center = 310*0.5f; // CenterPoleの座標 * Filed1のスケール
+
         for (int i = 0; i < bombItemNum; i++) {
             Addressables.InstantiateAsync("Assets/Prefabs/BombItem.prefab", 
-                new Vector3(Random.value * 250, 0.5f, Random.value * 250), 
+                new Vector3(Random.Range(center-range, center+range), 0.5f, Random.Range(center-range, center+range)), 
                 this.transform.rotation);
         }
         for (int i = 0; i < cannonItemNum; i++) {
             Addressables.InstantiateAsync("Assets/Prefabs/CannonItem.prefab", 
-                new Vector3(Random.value * 250, 0.5f, Random.value * 250), 
+                new Vector3(Random.Range(center-range, center+range), 0.5f, Random.Range(center-range, center+range)), 
                 this.transform.rotation);
         }
         for (int i = 0; i < recoverItemNum; i++) {
             Addressables.InstantiateAsync("Assets/Prefabs/RecoverItem.prefab", 
-                new Vector3(Random.value * 250, 0.5f, Random.value * 250), 
+                new Vector3(Random.Range(center-range, center+range), 0.5f, Random.Range(center-range, center+range)), 
                 this.transform.rotation);
         }           
     }
