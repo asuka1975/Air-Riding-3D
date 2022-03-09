27307ab0a783c57d4de03657e9bdd928837ce2d2
cb244 <cherica2132@gmail.com>
2022-03-09 23:26:05 +0900

    feat: マシン選択画面のマシンボタンのSEを追加

--- Assets/Scripts/MachineSelectScene/MachineSelect.cs
+++ Assets/Scripts/MachineSelectScene/MachineSelect.cs
@@ -7,14 +7,19 @@ using UnityEngine.UI;
 public class MachineSelect : MonoBehaviour
 {
     public int id;
-
+    public AudioClip SE_click_button;
+    public AudioClip SE_hover_button;
     public void OnClick()
     {
-
+        GameObject.Find("SoundManager").GetComponent<AudioSource>().PlayOneShot(SE_click_button);
         var machine = GameObject.Find("MachineSelectManager");
         machine.GetComponent<MachineSelectManager>().id = id;
         var str = "Selected machine ID is " + machine.GetComponent<MachineSelectManager>().id;
         Debug.Log(str);
         ButtonManager.SetInteractive("Button_StartGame", true);
+    }
+    public void PointerEnter()
+    {
+        GameObject.Find("SoundManager").GetComponent<AudioSource>().PlayOneShot(SE_hover_button);
     }
 }
