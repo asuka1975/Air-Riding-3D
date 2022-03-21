3af938edc3666b5ff25a05237205050cfa388634
cb244 <cherica2132@gmail.com>
2022-03-21 21:25:46 +0900

    feat: 全てのボタンにホバー音を実装

---
+++ Assets/Scripts/HoverSE.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class HoverSE : MonoBehaviour
+{
+    public AudioClip SE_hover;
+    public void PointerEnter()
+    {
+        GameObject.Find("SE").GetComponent<AudioSource>().PlayOneShot(SE_hover, 0.2f); // SE (hover)
+    }
+}
--- Assets/Scripts/MachineSelectScene/MachineSelect.cs
+++ Assets/Scripts/MachineSelectScene/MachineSelect.cs
@@ -9,7 +9,6 @@ public class MachineSelect : MonoBehaviour
 {
     public int id;
     public AudioClip SE_select;
-    public AudioClip SE_hover;
     public void OnClick()
     {
         GameObject.Find("SE").GetComponent<AudioSource>().PlayOneShot(SE_select, 0.5f); // SE (select)
@@ -18,9 +17,5 @@ public class MachineSelect : MonoBehaviour
         var str = "Selected machine ID is " + machine.GetComponent<MachineSelectManager>().id;
         Debug.Log(str);
         ButtonManager.SetInteractive("Button_StartGame", true);
-    }
-    public void PointerEnter()
-    {
-        GameObject.Find("SE").GetComponent<AudioSource>().PlayOneShot(SE_hover, 0.2f); // SE (hover)
     }
 }
